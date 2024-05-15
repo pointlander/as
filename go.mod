339f9ae087cc99bdaaa50ccf05969c358a1896db
Andrew Snodgrass <pointlander@gmail.com>
2024-05-15 18:15:01 -0500

    Added controller code

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/creack/goselect v0.1.2 // indirect
+	github.com/veandco/go-sdl2 v0.4.38 // indirect
 	go.bug.st/serial v1.6.2 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 )
--- main.go
+++ main.go
@@ -5,16 +5,167 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
 	"go.bug.st/serial"
 )
 
+var joysticks = make(map[int]*sdl.Joystick)
+
+type (
+	// JoystickState is the state of a joystick
+	JoystickState uint
+	// Mode is the operating mode of the robot
+	Mode uint
+	// Camera is a camera
+	TypeCamera uint
+)
+
+const (
+	// JoystickStateNone is the default state of a joystick
+	JoystickStateNone JoystickState = iota
+	// JoystickStateUp is the state of a joystick when it is pushed up
+	JoystickStateUp
+	// JoystickStateDown is the state of a joystick when it is pushed down
+	JoystickStateDown
+)
+
+const (
+	// ModeManual
+	ModeManual Mode = iota
+	// ModeAuto
+	ModeAuto
+)
+
+// String returns a string representation of the JoystickState
+func (j JoystickState) String() string {
+	switch j {
+	case JoystickStateUp:
+		return "up"
+	case JoystickStateDown:
+		return "down"
+	default:
+		return "none"
+	}
+}
+
 func main() {
-	mode := &serial.Mode{
+	options := &serial.Mode{
 		BaudRate: 115200,
 	}
-	port, err := serial.Open("/dev/ttyAMA0", mode)
+	port, err := serial.Open("/dev/ttyAMA0", options)
 	if err != nil {
 		panic(err)
 	}
 	_ = port
+
+	var event sdl.Event
+	var running bool
+	sdl.Init(sdl.INIT_JOYSTICK)
+	defer sdl.Quit()
+	sdl.JoystickEventState(sdl.ENABLE)
+	running = true
+	var axis [5]int16
+	joystickLeft := JoystickStateNone
+	joystickRight := JoystickStateNone
+	//var speed int16
+	var mode Mode
+
+	_, _ = joystickLeft, joystickRight
+	for running {
+		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch t := event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			case *sdl.JoyAxisEvent:
+				value := int16(t.Value)
+				axis[t.Axis] = value
+				if t.Axis == 3 || t.Axis == 4 {
+					if mode == ModeManual {
+						if axis[3] < 20000 && axis[3] > -20000 {
+							if axis[4] < -32000 {
+								joystickRight = JoystickStateUp
+							} else if axis[4] > 32000 {
+								joystickRight = JoystickStateDown
+							} else {
+								joystickRight = JoystickStateNone
+							}
+						} else {
+							joystickRight = JoystickStateNone
+						}
+					}
+					//fmt.Printf("right [%d ms] Which: %v \t%d %d\n",
+					//              t.Timestamp, t.Which, axis[3], axis[4])
+				} else if t.Axis == 0 || t.Axis == 1 {
+					if mode == ModeManual {
+						if axis[0] < 20000 && axis[0] > -20000 {
+							if axis[1] < -32000 {
+								joystickLeft = JoystickStateUp
+							} else if axis[1] > 32000 {
+								joystickLeft = JoystickStateDown
+							} else {
+								joystickLeft = JoystickStateNone
+							}
+						} else {
+							joystickLeft = JoystickStateNone
+						}
+					}
+					//fmt.Printf("left [%d ms] Which: %v \t%d %d\n",
+					//t.Timestamp, t.Which, axis[0], axis[1])
+				} else if t.Axis == 2 {
+					//fmt.Printf("2 axis [%d ms] Which: %v \t%x\n",
+					//      t.Timestamp, t.Which, value)
+					//speed = axis[2]
+					//pwm = int(100 * (float64(speed) + 32768) / 65535)
+					//fmt.Printf("speed %d pwm %d\n", speed, pwm)
+				}
+			case *sdl.JoyBallEvent:
+				fmt.Printf("[%d ms] Ball:%d\txrel:%d\tyrel:%d\n",
+					t.Timestamp, t.Ball, t.XRel, t.YRel)
+			case *sdl.JoyButtonEvent:
+				fmt.Printf("[%d ms] Button:%d\tstate:%d\n",
+					t.Timestamp, t.Button, t.State)
+				if t.Button == 0 && t.State == 1 {
+					switch mode {
+					case ModeManual:
+						mode = ModeAuto
+					case ModeAuto:
+						mode = ModeManual
+						joystickLeft = JoystickStateNone
+						joystickRight = JoystickStateNone
+					}
+				} else if t.Button == 1 && t.State == 1 {
+					//pwm = (pwm + 25) % 100
+				}
+			case *sdl.JoyHatEvent:
+				fmt.Printf("[%d ms] Hat:%d\tvalue:%d\n",
+					t.Timestamp, t.Hat, t.Value)
+				if t.Value == 1 {
+					// up
+				} else if t.Value == 4 {
+					// down
+				} else if t.Value == 8 {
+					// left
+				} else if t.Value == 2 {
+					// right
+				}
+			case *sdl.JoyDeviceAddedEvent:
+				fmt.Println(t.Which)
+				joysticks[int(t.Which)] = sdl.JoystickOpen(int(t.Which))
+				if joysticks[int(t.Which)] != nil {
+					fmt.Printf("Joystick %d connected\n", t.Which)
+				}
+			case *sdl.JoyDeviceRemovedEvent:
+				if joystick := joysticks[int(t.Which)]; joystick != nil {
+					joystick.Close()
+				}
+				fmt.Printf("Joystick %d disconnected\n", t.Which)
+			default:
+				fmt.Printf("Unknown event\n")
+			}
+		}
+
+		sdl.Delay(16)
+	}
 }
