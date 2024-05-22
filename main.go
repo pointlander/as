7976d3b79e82797bf1db28355d36d2da688068d1
Andrew Snodgrass <pointlander@gmail.com>
2024-05-22 17:49:35 -0500

    Refactor

--- main.go
+++ main.go
@@ -118,6 +118,68 @@ func softmax(values []float64, t float64) []float64 {
 	return output
 }
 
+// KMind is a kolmogorov complexity mind
+type KMind struct {
+	ActionBuffer []byte
+	ActionState  []byte
+	StateIndex   int
+	ActionIndex  int
+	Filter       []float64
+}
+
+// NewKMind creates a new kolmogorv mind
+func NewKMind(rng *rand.Rand) KMind {
+	actionBuffer := make([]byte, Size)
+	actionState := make([]byte, Size)
+	for i := range actionState {
+		actionState[i] = byte(rng.Intn(256))
+		actionBuffer[i] = byte(rng.Intn(256))
+	}
+	filter := make([]float64, ActionCount)
+	return KMind{
+		ActionBuffer: actionBuffer,
+		ActionState:  actionState,
+		StateIndex:   0,
+		ActionIndex:  1,
+		Filter:       filter,
+	}
+}
+
+func (k *KMind) Step(rng *rand.Rand, entropy float64) int {
+	k.StateIndex = (k.StateIndex + 2) % Size
+	k.ActionState[k.StateIndex] = byte(math.Round(entropy))
+	k.ActionIndex = (k.ActionIndex + 2) % Size
+	entropies := make([]float64, ActionCount)
+	for a := 0; a < int(ActionCount); a++ {
+		pre := byte(a)
+		for i, value := range k.ActionBuffer[:len(k.ActionBuffer)-1] {
+			k.ActionBuffer[i], pre = pre, value
+		}
+		output := bytes.Buffer{}
+		compress.Mark1Compress1(k.ActionBuffer, &output)
+		entropy := 256 * float64(output.Len()) / Size
+		k.ActionState[k.ActionIndex] = byte(math.Round(entropy))
+		output = bytes.Buffer{}
+		compress.Mark1Compress1(k.ActionState, &output)
+		entropies[a] = float64(output.Len()) / Size
+	}
+	for i, value := range entropies {
+		k.Filter[i] = (k.Filter[i] + value) / 2
+	}
+	normalized := softmax(k.Filter, .4)
+	sum, action, selected := 0.0, 0, rng.Float64()
+	for i, value := range normalized {
+		sum += value
+		if sum > selected {
+			action = i
+			break
+		}
+	}
+	k.ActionState[k.ActionIndex] = byte(math.Round(256 * entropies[action]))
+	k.ActionBuffer[0] = byte(action)
+	return action
+}
+
 func main() {
 	options := &serial.Mode{
 		BaudRate: 115200,
@@ -146,16 +208,8 @@ func main() {
 	go camera.Start("/dev/video0")
 	go func() {
 		rng := rand.New(rand.NewSource(1))
-		actionBuffer := make([]byte, Size)
 		var imgBuffer *dsputils.Matrix
-		actionState := make([]byte, Size)
-		stateIndex := 0
-		actionIndex := 1
-		for i := range actionState {
-			actionState[i] = byte(rng.Intn(256))
-			actionBuffer[i] = byte(rng.Intn(256))
-		}
-		filter := make([]float64, ActionCount)
+		mind := NewKMind(rng)
 		for img := range camera.Images {
 			dx := img.Gray.Bounds().Dx()
 			dy := img.Gray.Bounds().Dy()
@@ -194,37 +248,7 @@ func main() {
 				}
 			}
 			entropy = -entropy * 4
-			stateIndex = (stateIndex + 2) % Size
-			actionState[stateIndex] = byte(math.Round(entropy))
-			actionIndex = (actionIndex + 2) % Size
-			entropies := make([]float64, ActionCount)
-			for a := 0; a < int(ActionCount); a++ {
-				pre := byte(a)
-				for i, value := range actionBuffer[:len(actionBuffer)-1] {
-					actionBuffer[i], pre = pre, value
-				}
-				output := bytes.Buffer{}
-				compress.Mark1Compress1(actionBuffer, &output)
-				entropy := 256 * float64(output.Len()) / Size
-				actionState[actionIndex] = byte(math.Round(entropy))
-				output = bytes.Buffer{}
-				compress.Mark1Compress1(actionState, &output)
-				entropies[a] = float64(output.Len()) / Size
-			}
-			for i, value := range entropies {
-				filter[i] = (filter[i] + value) / 2
-			}
-			normalized := softmax(filter, .4)
-			sum, action, selected := 0.0, 0, rng.Float64()
-			for i, value := range normalized {
-				sum += value
-				if sum > selected {
-					action = i
-					break
-				}
-			}
-			actionState[actionIndex] = byte(math.Round(256 * entropies[action]))
-			actionBuffer[0] = byte(action)
+			action := mind.Step(rng, entropy)
 			a = TypeAction(action)
 		}
 	}()
